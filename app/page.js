114d246a10d5477f7ca1ed97d3cc3188687ca7a4
Oliver <opaucarq@uni.pe>
2023-09-18 11:20:43 -0500

    update: main page - main structure

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
-export default function Home() {
+import { getAllTodos } from "@/api";
+import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
+
+export default async function Home() {
+  const tasks = await getAllTodos()
+  console.log(tasks)
   return (
-    <main>
-      <h1>Home Page</h1>
+    <main className="max-w-4xl mx-auto mt-4">
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">ToDo List App</h1>
+        <AddTask/>
+      </div>
+      <TodoList tasks={tasks}/>
     </main>
   )
 }
